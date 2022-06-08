1c50ec18ed513508bcad1b7abdf02cb9cdbc4750
minh <hkminh1306@gmail.com>
2022-06-08 14:07:24 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/client/header/header.component';
+import { BannerComponent } from './layouts/client/banner/banner.component';
+import { ContentComponent } from './layouts/client/content/content.component';
+import { FooterComponent } from './layouts/client/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
